5f4755ccba17c62d01bf356ed97e348e3ed2b2a6
Savely <olenev.savely@yandex.by>
2022-09-26 22:20:44 +0400

    Add Files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            F();
+        }
+
+        static void F()
+        {
+            i = 1;
+            Console.WriteLine(i);
+        }
+        static int i = 0;
+
+    }
+
+}
